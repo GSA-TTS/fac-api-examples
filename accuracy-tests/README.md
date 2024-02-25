2e5164ed49834d96ba8df9b709927f84996ed7f2
Matt Jadud <matthew.jadud@gsa.gov>
2024-02-25 11:57:26 -0500

    Cleaned up a bit.
    Specifically, a few notes in the readme. `main` wants to have a better
    output format.

---
+++ accuracy-tests/README.md
@@ -0,0 +1,7 @@
+# accuracy-tests
+
+This code, as written, currently requires a local copy of the FAC running, with public data loaded from 2016-2022.
+
+A future version may operate on local/exported SQLite database files or some other source.
+
+It can be modified to operate against the live FAC data (api.fac.gov), but it will run up against key request limits that are not present when running locally.
--- accuracy-tests/main.py
+++ accuracy-tests/main.py
@@ -2,20 +2,12 @@ from aln.checks import (
     accuracy_alns,
     categorize_alns
     )
-import logging
 from pprint import pprint
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("ALNs")
-
-MAX_RESULTS = 4_000_000
-STEP_SIZE = 19_000
-AUDITS_TO_CHECK=50_000
-
-
 if __name__ in "__main__":
-    categories = categorize_alns(2022)
-    pprint(categories)
-    valid, total = accuracy_alns(2022)
-    print({"valid": valid, "total": total})
-    
+    for year in [16, 17, 18, 19, 20, 21, 22]:
+        categories = categorize_alns(2000+year)
+        pprint(categories)
+        valid, total = accuracy_alns(2000+year)
+        print({"valid": valid, "total": total})
+     
