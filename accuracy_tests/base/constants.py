0a7e42707a4bc972562d445451144bc04f5fdead
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2025-08-12 11:52:17 -0400

    Url fix?

--- accuracy_tests/base/constants.py
+++ accuracy_tests/base/constants.py
@@ -1,7 +1,7 @@
 import os
 
 FAC_API_BASE = os.getenv("FAC_API_URL")
-FAC_API_BASE = "http://localhost:3000"
+#FAC_API_BASE = "http://localhost:3000"
 FAC_API_KEY = os.getenv("API_GOV_KEY")
 
 BASE_HEADERS = {
